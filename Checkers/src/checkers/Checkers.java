a77e20631438b9de0624dc92e0f5555136d7b685
Swarzshanaggen <54327694+Swarzshanaggen@users.noreply.github.com>
2019-12-22 14:34:37 -0600

    Initial Repository
    Made an initial repository, with images to be used for gui and a working java program

---
+++ Checkers/src/checkers/Checkers.java
@@ -0,0 +1,18 @@
+package checkers;
+
+/**
+ *
+ * @author Mitchell Ingham
+ * 
+ * This is a program that lets you play against a checkers AI, made by me
+ * 
+ */
+public class Checkers {
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is a test");
+        
+    }
+    
+}
